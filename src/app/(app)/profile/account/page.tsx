070eab5be374c9c2880a430ffa6022cbde8a3ab2
niche-markets <horchdmit62692@gmail.com>
2026-05-29 23:32:10 -0500

    initialize the project

---
+++ src/app/(app)/profile/account/page.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Metadata } from 'next';
+// import Account  from '@/views/profile/account';
+import dynamic from 'next/dynamic'
+export const metadata: Metadata = {
+    title: "Terafarm | Account",
+    description: "This is Account Page"
+};
+const Account = dynamic(() => import('@/views/profile/account'), { ssr: false })
+
+const AccountyPage = () => {
+    return (
+        <Account />
+    );
+};
+
+export default AccountyPage;
